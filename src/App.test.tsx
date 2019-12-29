cb52a2307616af12b3bd737800b4dbd457e1f976
David Wilson <spamdog@gmail.com>
2019-12-29 17:21:00 +1000

    Updated tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders title', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/D&D 5e Character Sheet Maker/i);
   expect(linkElement).toBeInTheDocument();
 });
